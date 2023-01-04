39b4efc23b06ad72a0540270193d905a54d1684d
abrhamgg <abrhamgg@gmail.com>
2023-01-04 22:50:26 +0300

    pep8

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,31 +1,20 @@
 #!/usr/bin/python3
-"""locked boxes"""
+"""
+unlock all the boxes
+The first box is open
+and every box has a key to another box
+"""
+
 
 def canUnlockAll(boxes):
-    """lockedbox algorithm
-    to identify if all boxes can be opened
-    :param boxes: list of list
-    :return: booleans
-    """
-    if boxes is None:
+    """ find every key to open all """
+    size = len(boxes)
+    keys = [0]
+    for i in keys:
+        for key in boxes[i]:
+            if key not in keys and key < size:
+                keys.append(key)
+    if len(keys) == size:
+        return True
+    else:
         return False
-    
-    all_boxes = []
-    keys = boxes[0]
-    not_opened = []
-    for i in range(1, len(boxes) - 1):
-        all_boxes.append(i)
-        if i in keys:
-            for j in boxes[i]:
-                if j not in keys:
-                    keys.append(j)
-        else:
-            not_opened.append(i)
-    for i in not_opened:
-        for j in boxes[i]:
-            if j not in keys:
-                keys.append(j)
-    for i in all_boxes:
-        if i not in keys:
-            return False
-    return True
