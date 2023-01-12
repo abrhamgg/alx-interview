f0f71250a8551ae919d39efad9f06594eb513e93
abrhamgg <abrhamgg@gmail.com>
2023-01-12 22:54:58 +0300

    task 0- completed

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,17 +11,14 @@ def minOperations(n):
     calculates the fewest number of operations needed to result in
     exactly n H characters in the file."""
 
-    factor = 0
-    if n <= 1:
+    if n < 2 or not isinstance(n, int):
         return 0
-    for i in range(2, int(n/2)+1):
-        if (n % i) == 0:
-            factor += 1
-
-    if factor == 0:
-        return n
-    op = math.log(n, 2)
-    if (n % op == 0):
-        return int(op) * 2
-    if int(op) >= 1:
-        return (int(op) * 2) + round(op - 2)
+    count = 1
+    final = []
+    while n != 1:
+        count += 1
+        if n % count == 0:
+            while (n % count == 0 and n != 1):
+                n /= count
+                final.append(count)
+    return sum(final)
