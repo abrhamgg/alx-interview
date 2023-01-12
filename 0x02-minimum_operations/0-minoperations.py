fb7629e2877c202aa372e901a609479559f07d71
abrhamgg <abrhamgg@gmail.com>
2023-01-12 22:11:36 +0300

    task 1 completed

---
+++ 0x02-minimum_operations/0-minoperations.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+"""minimum operation"""
+import math
+
+
+def minOperations(n):
+    """In a text file, there is a single character H.
+    Your text editor can execute only two operations in this file:
+    Copy All and Paste. Given a number n, write a method that
+    calculates the fewest number of operations needed to result in
+    exactly n H characters in the file."""
+
+    if n <= 1:
+        return 0
+    op = math.log(n, 2)
+    if int(op) - 2 >= 1:
+        return (int(op) * 2) + 1
+    return int(op) * 2
