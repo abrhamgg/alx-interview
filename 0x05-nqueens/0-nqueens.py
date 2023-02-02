be27f0d695bc86fa178ddcf768507de7ee945dbf
abrhamgg <abrhamgg@gmail.com>
2023-02-02 21:34:15 +0300

    completed task

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -2,88 +2,45 @@
 """nqueens"""
 import sys
 
-result = []
 
-
-def is_pos_safe(board, row, col):
-    """function to check if position is safe
-    place a queen"""
-
-    '# check rows on left'
-    for i in range(col):
-        if board[row][i]:
+def is_safe(solution, pos):
+    """Function to check if a queen can be placed
+    at a given position"""
+    for q in solution:
+        if q[1] == pos[1]:
             return False
-
-    '# check upper diagonal on the left'
-    y = row
-    x = col
-    while y >= 0 and x >= 0:
-        if board[y][x]:
+        if (q[0] + q[1]) == (pos[0] + pos[1]):
             return False
-        y -= 1
-        x -= 1
-
-    y = row
-    x = col
-    while x >= 0 and y < 4:
-        if(board[y][x]):
+        if (q[0] - q[1]) == (pos[0] - pos[1]):
             return False
-        y = y + 1
-        x = x - 1
     return True
 
 
-def n_queens(board, col, size):
-    """n queens"""
-    if col == size:
-        v = []
-        for i in range(len(board)):
-            for j in range(len(board)):
-                if board[i][j] == 1:
-                    new = [i, j]
-                    v.append(new)
-        if v not in result:
-            result.append(v)
-        return True
-
-    res = False
-
-    for i in range(size):
-        if is_pos_safe(board, i, col):
-            board[i][col] = 1
-
-            res = n_queens(board, col + 1, size) or res
-            board[i][col] = 0
-    return res
+def queens(row, n, solution):
+    """function that finds solution recursively
+    in every row"""
+    if row == n:
+        print(solution)
+    else:
+        for col in range(n):
+            pos = [row, col]
+            if is_safe(solution, pos):
+                solution.append(pos)
+                queens(row + 1, n, solution)
+                solution.remove(pos)
 
 
-def solveNQ(n):
-    result.clear()
-    board = [[0 for j in range(n)]
-             for i in range(n)]
-    n_queens(board, 0, n)
-    return result
-
-
-memory = []
-n = 0
-if len(sys.argv) != 2:
-    print("Usage: nqueens N")
-    sys.exit(1)
-try:
-    n = int(sys.argv[1])
-except Exception:
-    print("N must be a number")
-    sys.exit(1)
-if n < 4:
-    print("N must be at least 4")
-    sys.exit(1)
-
-res = solveNQ(n)
-'''first = res[0]
-memory.append(first[0])
-print(first)'''
-for i in res:
-    if i[0] not in memory:
-        print(i)
-        '''memory.append(i[0])'''
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: nqueens")
+        sys.exit(1)
+    try:
+        n = int(sys.argv[1])
+    except Exception:
+        print('N must be a number')
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    solution = []
+    queens(0, n, solution)
